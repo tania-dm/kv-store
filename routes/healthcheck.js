c25f1b5b751574c2521467e4145d5ed10192231f
tania <t.dimache@viriciti.com>
2021-02-12 14:20:57 +0100

    Add healthcheck endpoint:
    - generate api documentation using swagger
    - serialize data with json schema

---
+++ routes/healthcheck.js
@@ -0,0 +1,24 @@
+'use strict'
+
+module.exports = async function (fastify, opts) {
+  fastify.route({
+    method: 'GET',
+    url: '/',
+    schema: {
+      tags: ['Healthcheck'],
+      description: 'Healthcheck endpoint to verify if the app is running propertly',
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            status: { type: 'string' },
+            timestamp: { type: 'string', format: 'date-time' }
+          }
+        }
+      }
+    },
+    handler: async (request, reply) => {
+      return { status: 'ok', timestamp: new Date().toISOString() };
+    }
+  })
+}
